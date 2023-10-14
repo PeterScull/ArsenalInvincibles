df298d03c96c2aaca8005404d0295cb2a3c08879
PeterScull <141519546+PeterScull@users.noreply.github.com>
2023-10-14 15:56:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 from flask import Flask
-
-
 app = Flask(__name__)
 
 @app.route('/')
 def home():
- return "<h1 style='text-align: center;'>We Are The Invincibles</h1>"
+    return "<h1 style='text-align: center;'>welcome to the invincibles</h1>"
 
-if __name__ == "__main__":
- app.run(host='0.0.0.0', port=80, debug=True) #port=80
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True) # specify port=80
