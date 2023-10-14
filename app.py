900755c745a2249beb2035db76b1c44fa0e2e792
PeterScull <141519546+PeterScull@users.noreply.github.com>
2023-10-14 15:55:57 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
 from flask import Flask
-import logging
-from flask.logging import create_logger
 
 
 app = Flask(__name__)
-LOG = create_logger(app)
-LOG.setLevel(logging.INFO)
 
 @app.route('/')
 def home():
  return "<h1 style='text-align: center;'>We Are The Invincibles</h1>"
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=80, debug=True) #port=80
+ app.run(host='0.0.0.0', port=80, debug=True) #port=80
