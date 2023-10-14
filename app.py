ca55dae3d2e6cfef2f6082502d27abb682592bf9
PeterScull <141519546+PeterScull@users.noreply.github.com>
2023-10-14 15:02:16 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,11 +9,12 @@ LOG.setLevel(logging.INFO)
 
 @app.route('/')
 def home():
-	header = "
 	<h2 style='text-align: center;'> The greatest team to EVER: The Arsenal Invincibles </h2>"
-	paragraph = "
-	<p> The invincibles had a record of 24 wins and 12 draws and 0 loses. No one has come close to this record, many have attempted but none have succeeded. </p>"
-	return header + paragraph
+	<p> The invincibles had a record of 24 wins and 12 draws and 0 loses. </br> 
+	No one has come close to this record, many have attempted but none have succeeded. undefined</br>
+	The spine of the team was formed by the likes of Patrick Vieira, Thierry Henry, Robert Pires, Sol Campbell, Dennis Bergkamp, and Ashley Cole. </br>
+	These players, along with others such as Gilberto Silva, Lauren, Freddie Ljungberg, and Jens Lehmann, formed a formidable unit that possessed both flair and work rate.
+	</p>"
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=80, debug=True) #port=80
