e4a965e8298410d2dce67ff2b012695c3d005a3b
PeterScull <141519546+PeterScull@users.noreply.github.com>
2023-10-14 14:55:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,9 @@ LOG.setLevel(logging.INFO)
 
 @app.route('/')
 def home():
-return "<h2 style='text-align: center;'> The greatest team to EVER: The Arsenal Invincibles </h2>"
-return "<p> The invincibles had a record of 24 wins and 12 draws and 0 losses. No one has come close to this record, many have attempted but none have succeeded. </p>"
+	header = "<h2 style='text-align: center;'> The greatest team to EVER: The Arsenal Invincibles </h2>"
+	paragraph= "<p> The invincibles had a record of 24 wins and 12 draws and 0 losses. No one has come close to this record, many have attempted but none have succeeded. </p>"
+return header + paragraph
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=80, debug=True) #port=80
